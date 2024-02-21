2452754b85a7273cec4d4896a467b94581e946e4
Krzysztof Stepien <krzysztofstepien90@gmail.com>
2024-02-21 17:42:17 +0100

    Crated main components without full logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
-import Button from "./components/UI/Button";
-import Form from "./components/UI/Form";
-import Input from "./components/UI/Input";
-import React from 'react'
-import { type FormHandle } from "./components/UI/Form";
-function App() {
-  const customRef = React.useRef<FormHandle>(null)
+import AddTimer from './components/AddTimer.tsx';
+import Header from './components/Header.tsx';
+import Timers from './components/Timers.tsx';
 
-  function handleSave(data: unknown) {
-    const extractedData = data as { name: string, id: number }
-    console.log(extractedData)
-    customRef.current?.clear()
-  }
+function App() {
   return (
-    <main>
-      <Form onSave={handleSave} ref={customRef}>
-        <Input label="Name" id="name" type="text" />
-        <Input label="Age" id="age" type="number" />
-        <p>
-          <Button>Click me!</Button>
-        </p>
-      </Form>
-    </main>
-  )
+    <>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </>
+  );
 }
 
 export default App;
