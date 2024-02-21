ebe20e72b0b9349dbe902969361f8dbe186526b0
Krzysztof Stepien <krzysztofstepien90@gmail.com>
2024-02-21 18:06:58 +0100

    created custom hook to provide context value

--- src/store/timers-context.tsx
+++ src/store/timers-context.tsx
@@ -1,4 +1,5 @@
-import { createContext } from "react";
+import { type ReactNode, createContext, useContext } from "react";
+import React from 'react'
 
 type Timer = {
     name: string,
@@ -10,6 +11,11 @@ type TimersState = {
     timers: Timer[]
 }
 
+const initialState: TimersState = {
+    isRunning: false,
+    timers: []
+}
+
 type TimersContextValue = TimersState & {
     addTimer: (timerData: Timer) => void
     startTimers: () => void
@@ -17,3 +23,38 @@ type TimersContextValue = TimersState & {
 }
 
 const TimersContext = createContext<TimersContextValue | null>(null)
+export function useTimersContext() {
+    const timersCtx = useContext(TimersContext)
+    if (timersCtx === null) {
+        throw new Error('TimersContext is null - that should not be the case!')
+    }
+    return timersCtx
+}
+
+type TimersContextProviderProps = {
+    children: ReactNode
+}
+
+export default function TimersContextProvider({ children }: TimersContextProviderProps) {
+
+    const [,] = React.useReducer(reducer, initialState)
+
+    const ctx: TimersContextValue = {
+        timers: [],
+        isRunning: false,
+        addTimer: (timerData) => {
+
+        },
+        startTimers: () => {
+
+        },
+        stopTimers: () => {
+
+        }
+    }
+    return (
+        <TimersContext.Provider value={ctx}>
+            {children}
+        </TimersContext.Provider>
+    )
+}
